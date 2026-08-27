5bb15bda118d71b3f3c56a3be30990021b62c592
PrateekMishraaa <pm921670@gmail.com>
2026-08-27 13:45:46 +0530

    fix

--- src/pages/Home/Homepage.jsx
+++ src/pages/Home/Homepage.jsx
@@ -1,15 +1,15 @@
 // src/pages/home/Homepage.jsx
 import React from 'react';
 import Hero from '../../components/home/Hero.jsx';
-import Services from '../../components/home/Services.jsx';
-import HowItWorks from '../../components/home/HowItWorks.jsx';
-import Testimonials from '../../components/home/Testimonials.jsx';
-import CTASection from '../../components/home/CTASection.jsx';
-import Stats from '../../components/home/Stats.jsx';
-import Features from '../../components/home/Features.jsx';
-import Pricing from '../../components/home/Pricing.jsx';
-import Partners from '../../components/home/Partners.jsx';
-import BlogPreview from '../../components/home/BlogPreview.jsx';
+import Services from '../../components/Home/Services.jsx';
+import HowItWorks from '../../components/Home/HowItWorks.jsx';
+import Testimonials from '../../components/Home/Testimonials.jsx';
+import CTASection from '../../components/Home/CTASection.jsx';
+import Stats from '../../components/Home/Stats.jsx';
+import Features from '../../components/Home/Features.jsx';
+import Pricing from '../../components/Home/Pricing.jsx';
+import Partners from '../../components/Home/Partners.jsx';
+import BlogPreview from '../../components/Home/BlogPreview.jsx';
 
 const Homepage = () => {
   return (
